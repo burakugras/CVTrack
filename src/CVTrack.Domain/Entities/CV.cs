53dfbf29f7d87f53a2725055fc0009de90622a81
burakugras <burakugras67@gmail.com>
2025-06-11 23:29:51 +0300

    update entities

--- src/CVTrack.Domain/Entities/CV.cs
+++ src/CVTrack.Domain/Entities/CV.cs
@@ -4,10 +4,10 @@ public class CV
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
-    public string FileName { get; set; }
+    public string FileName { get; set; } = null!;
     public DateTime UploadDate { get; set; }
 
     // Navigation properties
-    public User User { get; set; }
+    public User User { get; set; } = null!;
     public ICollection<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
 }
